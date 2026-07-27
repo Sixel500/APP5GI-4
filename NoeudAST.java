626878c40375deadd39f8c099869c621ef58f5f9
Chris <crysisrogue@gmail.com>
2026-07-27 12:15:52 -0400

    revert changes

--- NoeudAST.java
+++ NoeudAST.java
@@ -17,64 +17,60 @@ public class NoeudAST extends ElemAST {
   }
 
 
-    /** Evaluation de noeud d'AST
-     */
-    public String EvalAST() {
-        // Si tout ce sous-arbre est purement numérique, on calcule directement sa valeur entière
-        if (this.getType() == Terminal.Type.NOMBRE) {
-            return String.valueOf(this.IntEvalAST());
-        }
-
-        // Sinon, c'est un nœud mixte : on évalue d'abord chaque enfant séparément
-        String evalG = this.gauche.EvalAST();
-        String evalD = this.droite.EvalAST();
-
-        // Si par hasard les deux enfants (après évaluation) sont devenus des nombres purs,
-        // on peut tenter de faire le calcul localement
-        if (estUnNombre(evalG) && estUnNombre(evalD)) {
-            int g = Integer.parseInt(evalG);
-            int d = Integer.parseInt(evalD);
-            return String.valueOf(calculer(g, d));
-        }
-
-        // Sinon, on concatène la représentation symbolique
-        return evalG + this.operateur + evalD;
-    }
+  /** Evaluation de noeud d'AST
+   */
+  public String EvalAST( ) {
+     switch (this.operateur) {
+         case "+":
+             if (this.getType() == Terminal.Type.NOMBRE){
+                 return String.valueOf(this.gauche.IntEvalAST() + this.droite.IntEvalAST());
+             }else{
+                 return this.gauche.StringEvalAST() + "+" + this.droite.StringEvalAST();
+             }
+         case "-":
+             if (this.getType() == Terminal.Type.NOMBRE){
+                 return String.valueOf(this.gauche.IntEvalAST() - this.droite.IntEvalAST());
+             }else{
+                 return this.gauche.StringEvalAST() + "-" + this.droite.StringEvalAST();
+             }
+         case "*":
+             if (this.getType() == Terminal.Type.NOMBRE){
+                 return String.valueOf(this.gauche.IntEvalAST() * this.droite.IntEvalAST());
+             }else{
+                 return this.gauche.StringEvalAST() + "*" + this.droite.StringEvalAST();
+             }
+         case "/":
+             if (this.getType() == Terminal.Type.NOMBRE){
+                 return String.valueOf(this.gauche.IntEvalAST() / this.droite.IntEvalAST());
+             }else{
+                 return this.gauche.StringEvalAST() + "/" + this.droite.StringEvalAST();
+             }
+         default:
+             ErreurEvalAST("Opérateur non supporté : " + this.operateur);
+             return "er";
+     }
+  }
 
     @Override
     public int IntEvalAST() {
-        return calculer(this.gauche.IntEvalAST(), this.droite.IntEvalAST());
-    }
-
-    /** Méthode utilitaire pour effectuer le calcul selon l'opérateur */
-    private int calculer(int g, int d) {
         switch (this.operateur) {
-            case "+": return g + d;
-            case "-": return g - d;
-            case "*": return g * d;
+            case "+":
+                return this.gauche.IntEvalAST() + this.droite.IntEvalAST();
+            case "-":
+                return this.gauche.IntEvalAST() - this.droite.IntEvalAST();
+            case "*":
+                return this.gauche.IntEvalAST() * this.droite.IntEvalAST();
             case "/":
-                if (d == 0) ErreurEvalAST("Division par zéro");
-                return g / d;
+                    return this.gauche.IntEvalAST() / this.droite.IntEvalAST();
             default:
                 ErreurEvalAST("Opérateur non supporté : " + this.operateur);
                 return 0;
         }
     }
 
-    /** Vérifie si une chaîne représente un entier */
-    private boolean estUnNombre(String s) {
-        if (s == null || s.isEmpty()) return false;
-        try {
-            Integer.parseInt(s);
-            return true;
-        } catch (NumberFormatException e) {
-            return false;
-        }
-    }
-
     @Override
     public Terminal.Type getType() {
-        if (this.gauche.getType() == Terminal.Type.NOMBRE && this.droite.getType() == Terminal.Type.NOMBRE) {
+        if (this.gauche.getType() == Terminal.Type.NOMBRE && this.droite.getType() == Terminal.Type.NOMBRE){
             return Terminal.Type.NOMBRE;
         }
         return Terminal.Type.OPERANDE;
@@ -84,6 +80,7 @@ public class NoeudAST extends ElemAST {
     public String StringEvalAST() {
         return this.gauche.StringEvalAST() + this.operateur + this.droite.StringEvalAST();
     }
+
 
     /** Lecture de noeud d'AST
    */
